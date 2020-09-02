9892ec064e45fdbcd83baf20794790bf7d6b68fc
Himadri <himadris@gmail.com>
2020-09-01 18:56:12 -0700

    added pages, navbar component, and routes for app.j

---
+++ client/src/Components/Navbar/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./style.css";
+
+function Navbar(props) {
+    const links = ["Signup/Login", "Main Page", "Store", "Stage 1", "Boss"];
+    const navLinks = links.map(link => {
+        return (
+            <li><a href={"/" + link}>{link}</a></li>
+        )
+    });
+
+    return (
+        <nav>
+            <h2 className="logo"> <a href="/">{props.logoTitle}</a></h2>
+            <ul>{navLinks}</ul>
+        </nav>
+
+    )
+};
+
+export default Navbar;
