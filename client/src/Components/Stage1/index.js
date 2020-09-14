82c81768f63d684e0f326fb2ac901e3dd3afc128
Yev Ro <yev.rokhin@gmail.com>
2020-09-14 13:29:25 -0700

    Stage1 and Boss fight almost complete - a lot of bugs that need workign out but progress has been made

--- client/src/Components/Stage1/index.js
+++ client/src/Components/Stage1/index.js
@@ -18,11 +18,13 @@ import chocobo from "../../Assets/chocobo.gif"
 
 
 function Stage1Cards() {
+
+    // ================================== MONSTER ARRAY ==================================
     const monsterArray = [
         {
             name: "Orc",
             hp: 100,
-            attack: 50,
+            attack: 60,
             defense: 20,
             gil: 45,
             intro: "The savage Orc of Mount Kararak was separated from his horde.  His eyes began to glare as he sees you.  He is ready to crush you!",
@@ -31,7 +33,7 @@ function Stage1Cards() {
         {
             name: "Slime",
             hp: 75,
-            attack: 55,
+            attack: 45,
             defense: 15,
             gil: 45,
             intro: "Acidic slime that can melt the toughest armors if absorbed, starts jiggling towards you!",
@@ -40,8 +42,8 @@ function Stage1Cards() {
         {
             name: "Chocobooboo",
             hp: 150,
-            attack: 35,
-            defense: 10,
+            attack: 50,
+            defense: 20,
             gil: 45,
             intro: "This ain't your regular Honey Boo Boo...this is Chocobooboo! Chocobooboo glares at you with anger after noticing that you stepped on its food!",
             image: chocobo
@@ -49,7 +51,7 @@ function Stage1Cards() {
         {
             name: "Yevzilla",
             hp: 500,
-            attack: 25,
+            attack: 45,
             defense: 5,
             gil: 200,
             intro: "Yevzilla rises out of the ocean! Someone woke him from his slumber! If not defeated quickly, his lazer breath will destroy ALL!",
@@ -57,9 +59,9 @@ function Stage1Cards() {
         },
         {
             name: "Biala-lama",
-            hp: 50,
-            attack: 40,
-            defense: 40,
+            hp: 125,
+            attack: 55,
+            defense: 35,
             gil: 200,
             intro: "Biala-lama nears! Watch out for the elbows and knees of this creature - they are muaitastic!",
             image: chocobo
@@ -67,8 +69,8 @@ function Stage1Cards() {
         {
             name: "Himadrigotica",
             hp: 150,
-            attack: 30,
-            defense: 15,
+            attack: 45,
+            defense: 20,
             gil: 45,
             intro: "Himadrigotica is a unique species of a goat that can only be found in the Dark Forest and on Mount Kararak.  It has a nasty kick and an even nastier meeehhh-nsult!",
             image: chocobo
@@ -76,8 +78,8 @@ function Stage1Cards() {
         {
             name: "Garretpooficus",
             hp: 100,
-            attack: 10,
-            defense: 35,
+            attack: 45,
+            defense: 25,
             gil: 200,
             intro: "Not much is know about this creature except that it waits for its victims and then throws insults at them",
             image: chocobo
@@ -85,28 +87,40 @@ function Stage1Cards() {
         {
             name: "Kaiwhywhywhy",
             hp: 90,
-            attack: 35,
-            defense: 10,
+            attack: 45,
+            defense: 30,
             gil: 200,
             intro: "This creature will defeat you with its WHY WHY WHY attack!",
             image: chocobo
         }
     ]
+
+    // =============================== TEMP PLAYER STATS (TO BE DELETED LATER) ===================
+    const tempPlayer =
+    {
+        name: "Temporary Player",
+        hp: 150,
+        attack: 45,
+        defense: 22,
+        gil: 250,
+        potion: 5
+    };
+
+    const [tempPlayerStats, setTempPlayerStats] = useState(tempPlayer);
+
+    // ============================== GAME LOGIC ===============================================
     const [monsterStats, setMonsterStats] = useState(
         monsterArray[Math.floor(Math.random() * Math.floor(8))]
 
     );
-    const [win, setWin] = useState(
 
-    );
+    const [win, setWin] = useState();
 
-    const [lose, setLose] = useState(
+    const [lose, setLose] = useState();
 
-    );
-
-    const [userStats, setUserStats] = useState(
-
-    );
+    // ================================ USER/PLAYER LOGIC & INFO =====================================
+    // --------------------- Will pull info from db -------------------------------------
+    const [userStats, setUserStats] = useState();
 
     // useEffect(() => {
     //     const userId = localStorage.getItem("userId");
@@ -115,29 +129,61 @@ function Stage1Cards() {
     //     });
     // }, []);
 
+    // ==================================== COMBAT LOGIC =======================================
     const handleAttack = () => {
         console.log("attack")
-        const monsterHitPoints = combatAPI.attack(40, monsterStats.hp, monsterStats.defense);
+        const monsterHitPoints = combatAPI.attack(tempPlayerStats.attack, monsterStats.hp, monsterStats.defense);
         if (monsterHitPoints <= 0) {
             setWin("You Win");
             //add user gil after defeating monster
         } else {
-            setMonsterStats({ ...monsterStats, hp: monsterHitPoints })
+            setMonsterStats({ ...monsterStats, hp: monsterHitPoints });
+            const playerHitPoints = combatAPI.monsterRet(tempPlayerStats.hp, tempPlayerStats.defense, monsterStats.attack);
+            if (playerHitPoints <= 0) {
+                setLose("You Lose");
+            } else {
+                setTempPlayerStats({ ...tempPlayerStats, hp: playerHitPoints })
+            };
             // const monsterCounter = combatAPI.monsterRet(100, 20, monsterStats.attack);
             //setUserStats needed
-        }
-    }
+        };
+    };
 
     const handleGuard = () => {
         console.log("guard")
-        const userHitPoints = combatAPI.guard(100, 25, monsterStats.attack);
-        if (userHitPoints <= 0) {
+        const playerHitPoints = combatAPI.guard(tempPlayerStats.hp, tempPlayerStats.defense, monsterStats.attack);
+        if (playerHitPoints <= 0) {
             setLose("You Lose");
         } else {
-            // setUserStats({})
-            // setMonsterStats({ ...monsterStats, hp: monsterHitPoints });
+            setTempPlayerStats({ ...tempPlayerStats, hp: playerHitPoints });
+            const monsterHitPoints = combatAPI.attack(tempPlayerStats.attack, monsterStats.hp, monsterStats.defense);
+            if (monsterHitPoints <= 0) {
+                setWin("You Win");
+                //add user gil after defeating monster
+            } else {
+                setMonsterStats({ ...monsterStats, hp: monsterHitPoints });
+                // setMonsterStats({ ...monsterStats, hp: monsterHitPoints });
+            };
+        };
+    };
+
+    const handlePotion = () => {
+        console.log("potion")
+        const playerHitPoints = combatAPI.usePotion(tempPlayerStats.hp);
+        if (tempPlayerStats.potion >= 1) {
+            const playerPotions = combatAPI.reducePotions(tempPlayerStats.potion);
+            setTempPlayerStats({ ...tempPlayerStats, hp: playerHitPoints, potion: playerPotions });
+        } else {
+            //need to block out the potions button - look at store code
         }
-    }
+    };
+
+    const handleRun = () => {
+        alert("RUN YOU COWARD")
+    };
+
+
+    // ============================= REACT CARDS AND PAGE =====================================
     return (
         <Container-fluid>
             <Jumbotron>
@@ -149,23 +195,26 @@ function Stage1Cards() {
                         <Card style={{ width: '18rem' }} className="player">
                             <Card.Img variant="top" src={player} />
                             <Card.Body>
-                                <Card.Title>Player Name</Card.Title>
+                                <Card.Title>{tempPlayerStats.name}</Card.Title>
                                 <Card.Text>
+                                    You draw your Great Sword of Leeching (small chance to heal yourself during combat)!
+                                    <br />
+                                    <br />
                                     Prepare to fight the monster in front of you!
                                 </Card.Text>
                             </Card.Body>
                             <ListGroup className="list-group-flush stats">
-                                <ListGroupItem>HP: </ListGroupItem>
-                                <ListGroupItem>Attack: </ListGroupItem>
-                                <ListGroupItem>Defense: </ListGroupItem>
-                                <ListGroupItem>Potion: </ListGroupItem>
-                                <ListGroupItem>Gil: </ListGroupItem>
+                                <ListGroupItem>HP: {tempPlayerStats.hp}</ListGroupItem>
+                                <ListGroupItem>Attack: {tempPlayerStats.attack}</ListGroupItem>
+                                <ListGroupItem>Defense: {tempPlayerStats.defense}</ListGroupItem>
+                                <ListGroupItem>Potion: {tempPlayerStats.potion}</ListGroupItem>
+                                <ListGroupItem>Gil: {tempPlayerStats.gil}</ListGroupItem>
                             </ListGroup>
                             <ListGroup className="list-group-flush">
                                 <ListGroupItem><Button variant="danger" onClick={handleAttack}>Attack</Button></ListGroupItem>
                                 <ListGroupItem><Button variant="warning" onClick={handleGuard}>Guard</Button></ListGroupItem>
-                                <ListGroupItem><Button variant="success">Potion</Button></ListGroupItem>
-                                <ListGroupItem><Button variant="info">Run!</Button></ListGroupItem>
+                                <ListGroupItem><Button variant="success" onClick={handlePotion}>Potion</Button></ListGroupItem>
+                                <ListGroupItem><Button variant="info" onClick={handleRun} >Run!</Button></ListGroupItem>
                             </ListGroup>
                             {/* <Card.Body>
                                 <Card.Link href="#">Card Link</Card.Link>
